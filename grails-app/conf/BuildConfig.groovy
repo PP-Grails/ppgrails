252dc1b56861b215d83ab81282a1789193ab5dba
Paradigmas <jonatasmm@gmail.com>
2014-09-04 11:04:30 -0300

    Comentando boostrap

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -49,7 +49,7 @@ grails.project.dependency.resolution = {
         runtime ":hibernate:$grailsVersion"
         runtime ":jquery:1.8.3"
         runtime ":resources:1.2"
-        runtime ":twitter-bootstrap:3.1.1.3"
+       // runtime ":twitter-bootstrap:3.1.1.3"
 
         // Uncomment these (or add new ones) to enable additional resources capabilities
         //runtime ":zipped-resources:1.0"
@@ -61,6 +61,6 @@ grails.project.dependency.resolution = {
         runtime ":database-migration:1.3.2"
 
         compile ':cache:1.0.1'
-        compile ":twitter-bootstrap:3.2.0.2"
+     //   compile ":twitter-bootstrap:3.2.0.2"
     }
 }
